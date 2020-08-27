3439674423860dec28a340b471b379bd6086e84e
Amitesh <amitesh@Admins-MacBook-Pro.local>
2020-08-27 14:02:55 +0530

    Crud implemented

---
+++ resources/views/base.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Laravel 7 & MySQL CRUD Tutorial</title>
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+  <div class="container">
+    @yield('main')
+  </div>
+  <script src="{{ asset('js/app.js') }}" type="text/js"></script>
+  <script src="{{ asset('js/bootstrap.js') }}" type="text/js"></script>
+</body>
+</html>
